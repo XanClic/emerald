92a54e70b4edd2035379d133b84463c002303331
Hanna Reitz <xanclic@xanclic.moe>
2013-01-28 21:54:42 +0100

    mruby version
    + optional (commented) checkout to an mruby version known to work in
      fetch-mruby.rb

--- fetch-mruby.rb
+++ fetch-mruby.rb
@@ -15,6 +15,10 @@ status('Cloning mruby git...')
 
 die unless system('git clone git://github.com/mruby/mruby.git')
 
+# If anything should break in future mruby versions, comment this line in
+# (on a different note: Update the hash as often as possible)
+# die unless system('cd mruby; git checkout 0410bb7623a27ac427f190e3144c85b6446bec05')
+
 
 status('Patching build_config.rb...')
 
