b8c828fbee471bb368b490df79267cec4e75fde1
Hanna Reitz <xanclic@xanclic.moe>
2013-01-29 14:52:17 +0100

    longjmp
    ! Fixed.

--- src/kernel/lib/setjmp/longjmp.asm
+++ src/kernel/lib/setjmp/longjmp.asm
@@ -9,11 +9,11 @@ mov   eax,[esp+0x08]
 mov   esp,[edx+0x00]
 add   esp,8
 push  edx
-push  dword [eax+0x04]
-mov   ebx,[eax+0x08]
-mov   esi,[eax+0x0C]
-mov   edi,[eax+0x10]
-mov   ebp,[eax+0x14]
+push  dword [edx+0x04]
+mov   ebx,[edx+0x08]
+mov   esi,[edx+0x0C]
+mov   edi,[edx+0x10]
+mov   ebp,[edx+0x14]
 
 test  eax,eax
 jnz   return
