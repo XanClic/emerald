da02a27ec5e4d2d2a6ab38d766d2b0d8a6ea4c2f
Hanna Reitz <xanclic@xanclic.moe>
2013-01-29 17:08:35 +0100

    Exception
    + In theory, would now output an exception backtrace. In practice, mruby
      doesn't seem to generate one.

--- src/kernel/lib/stdio/fopen.c
+++ src/kernel/lib/stdio/fopen.c
@@ -9,8 +9,7 @@ extern mrb_state *pmrbs;
 
 FILE *fopen(const char *path, const char *mode)
 {
-    mrb_value ret = mrb_funcall_argv(pmrbs, mrb_nil_value(), mrb_intern(pmrbs, "fopen"), 2,
-                                     (mrb_value[]){ mrb_str_new_cstr(pmrbs, path), mrb_str_new_cstr(pmrbs, mode) });
+    mrb_value ret = mrb_funcall(pmrbs, mrb_nil_value(), "fopen", 2, mrb_str_new_cstr(pmrbs, path), mrb_str_new_cstr(pmrbs, mode));
 
     return (FILE *)mrb_fixnum(ret);
 }
