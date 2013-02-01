0f8bff345f486db45a37573917af5ad791d587e5
Hanna Reitz <xanclic@xanclic.moe>
2013-02-01 04:58:00 +0100

    MRB_INT64
    * Use 64 bit integers so there are no floating point operations just for
      setting up the GDT (which requires unsigned 32 bit integers).
    ! Fix INT..._MIN limits in stdint.h

--- fetch-mruby.rb
+++ fetch-mruby.rb
@@ -17,7 +17,7 @@ die unless system('git clone git://github.com/mruby/mruby.git')
 
 # If anything should break in future mruby versions, comment this line in
 # (on a different note: Update the hash as often as possible)
-# die unless system('cd mruby; git checkout e43216c45d537b456e8225b66e0c10b0703b31a0')
+# die unless system('cd mruby; git checkout 21310019639d7c6ec13b558ba73766121985950e')
 
 
 status('Patching build_config.rb...')
@@ -32,7 +32,7 @@ content = "# coding: utf-8\n\n" + content + <<-EOS
 MRuby::CrossBuild.new('emerald') do |conf|
   toolchain :gcc
 
-  conf.cc.flags << "-std=c11 -m32 -ffreestanding -nostartfiles -nostdinc -nodefaultlibs '-I#{Dir.pwd}/src/include' -DDISABLE_STDIO -DDISABLE_TIME"
+  conf.cc.flags << "-std=c11 -m32 -ffreestanding -nostartfiles -nostdinc -nodefaultlibs '-I#{Dir.pwd}/src/include' -DDISABLE_STDIO -DDISABLE_TIME -DMRB_INT64"
 
   conf.bins = %w()
 end
